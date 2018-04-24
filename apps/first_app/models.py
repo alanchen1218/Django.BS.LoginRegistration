68df60df0c24d41b9c88dd995d238a7d3c065529
Alan Chen <alanchen1218@gmail.com>
2018-04-24 01:49:28 -0700

    update

--- apps/first_app/models.py
+++ apps/first_app/models.py
@@ -7,20 +7,16 @@ import re
 class UserManager(models.Manager):
     def nameValidator(self, postData):
         errors = {}
-        if len(postData['first_name']) < 2:
-            errors['lengthFirstName'] = "First name must be at least 2 characters."
-        elif not re.match('[A-Za-z]+', postData['first_name']):
-            errors['firstNameValid'] = "First name must only contain letters."
-        if len(postData['last_name']) < 2:
-            errors['lengthLastName'] = "Last name must be at least 2 characters."
-        elif not re.match('[A-Za-z]+', postData['last_name']):
-            errors['lastNameValid'] = "Last name must only contain letters."
-        if len(postData['email']) < 1:
-            errors['lengthEmail'] = "Email is required."
-        elif not re.match('[A-Za-z-0-9-_]+(.[A-Za-z-0-9-_]+)*@[A-Za-z-0-9-_]+(.[A-Za-z-0-9-_]+)*(.[A-Za-z]{2,})', postData['email']):
-            errors['emailValid'] = "Email not valid."
-        elif User.objects.filter(email=postData['email']):
-            errors['emailTaken'] = "Email was already registered."
+        if len(postData['fullname']) < 2:
+            errors['lengthtName'] = "First name must be at least 2 characters."
+        elif not re.match('[A-Za-z]+', postData['fullname']):
+            errors['nameValid'] = "First name must only contain letters."
+
+        if len(postData['username']) < 1:
+            errors['lengthUsername'] = "username is required."
+        elif User.objects.filter(username=postData['username']):
+            errors['usernameTaken'] = "username was already registered."
+
         if len(postData['password']) < 8:
             errors['lengthPassword'] = "Password must be at least 8 characters."
         if postData['password'] != postData['confirm_password']:
@@ -29,25 +25,35 @@ class UserManager(models.Manager):
 
     def loginValidator(self, postData):
         errors = {}
-        if len(postData['email']) < 1:
-            errors['noEmail'] = "Please input an email."
-        elif not re.match('[A-Za-z-0-9-_]+(.[A-Za-z-0-9-_]+)*@[A-Za-z-0-9-_]+(.[A-Za-z-0-9-_]+)*(.[A-Za-z]{2,})', postData['email']):
-            errors['emaiValid'] = "Email is not valid."
-        elif not User.objects.get(email=postData['email']):
-            errors['emailExist'] = "This email is not registered in our database."
+        if len(postData['username']) < 1:
+            errors['noUser'] = "Please input an user."
+        elif not User.objects.filter(username=postData['username']):
+            errors['usernamelExist'] = "This username is not registered in our database."
+
         if len(postData['password']) < 1:
             errors['noPass'] = "Please input a password."
-        # elif len(postData['password']) < 8:
-        #     errors['short_pass'] = "Incorrect password: less than 8 characters."
-        elif bcrypt.checkpw(postData['password'].encode(), User.objects.get(email=postData['email']).password.encode()) == False:
+        elif bcrypt.checkpw(postData['password'].encode(), User.objects.get(username=postData['username']).password.encode()) == False:
             errors['incorrect_pass'] = "Incorrect password: does not match password stored in database."
         return errors
 
 class User(models.Model):
-    first_name = models.CharField(max_length = 255)
-    last_name = models.CharField(max_length = 255)
-    email = models.CharField(max_length = 255)
+    fullname = models.CharField(max_length = 255)
+    username = models.CharField(max_length = 255)
     password = models.CharField(max_length = 255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     objects = UserManager()
+
+class WishManager(models.Manager):
+    def WishManager(self, postData):
+        errors = {}
+        if len(postData['name']) < 3:
+            errors['name'] = 'Name must be at least 3 characters long'
+            return wrong
+
+class Wishlist(models.Model):
+    name = models.CharField(max_length=50)
+    added_by = models.ForeignKey(User, related_name='created')
+    created_at = models.DateTimeField(auto_now_add=True)
+    liked_users = models.ManyToManyField(User, related_name='liked_items')
+    objects = WishManager()
--- apps/first_app/urls.py
+++ apps/first_app/urls.py
@@ -2,7 +2,15 @@ from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
     url(r'^$', views.index),   # This line has changed! Notice that urlpatterns is a list, the comma is in
-    url(r'register$', views.register),
-    url(r'login$', views.login),
-    url(r'success$', views.success),
-]  
+    url(r'^register$', views.register),
+    url(r'^login$', views.login),
+    url(r'^dashboard$', views.dashboard),
+    url(r'wish_items/create$', views.create),
+    url(r'^createrender$', views.createrender),
+
+    url(r'^wish_items/(?P<number>\d+)$', views.wishItem), # NOT WORKING
+
+    url(r'^destroy/(?P<number>\d+)$', views.destroy),
+    # url(r'^add/(?P<number>\d+)$', views.add), NOT WORKING>>>>>>
+
+]
