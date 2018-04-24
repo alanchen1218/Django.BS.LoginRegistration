4ab6fc036389332a1bafdf984f98427a0087a9a1
Alan Chen <alanchen1218@gmail.com>
2018-04-24 02:05:26 -0700

    test

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
-from django.db.models import Q
+# from models import User, UserManager
 import bcrypt
 from .models import *
-
-# the index function is called when root is visited
+  # the index function is called when root is visited
 def index(request):
   return render(request, 'first_app/index.html')
 
@@ -13,14 +12,16 @@ def register(request):
   if len(errors):
     for key, value in errors.items():
       messages.error(request, value)
-      request.session['fullname'] = request.POST['fullname']
-      request.session['username'] = request.POST['username']
+      request.session['first_name'] = request.POST['first_name']
+      request.session['last_name'] = request.POST['last_name']
+      request.session['email'] = request.POST['email']
       return redirect('/')
   else:
     pwhash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
-    User.objects.create(fullname = request.POST['fullname'], username = request.POST['username'], password = pwhash)
-    request.session['fullname'] = request.POST['fullname']
-    return redirect('/dashboard')
+    User.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'], password = pwhash)
+    request.session['first_name'] = request.POST['first_name']
+    request.session['last_name'] = request.POST['last_name']
+    return redirect('/success')
 
 def login(request):
   errors = User.objects.loginValidator(request.POST)
@@ -29,59 +30,10 @@ def login(request):
       messages.error(request, value)
     return redirect('/')
   else:
-    request.session['fullname'] = User.objects.get(username=request.POST['username']).fullname
-    request.session['id'] = User.objects.get(username=request.POST['username']).id
-    print (request.session['id'])
-    return redirect('/dashboard')
+    request.session['first_name'] = User.objects.get(email=request.POST['email']).first_name
+    request.session['last_name'] = User.objects.get(email=request.POST['email']).last_name
+    return redirect('/success')
   
 
-def dashboard(request):
-  currentUser = User.objects.get(id = request.session['id'])
-  context = {
-    'wishlist' : Wishlist.objects.all(),
-    'otherliked' : Wishlist.objects.filter(~Q(liked_users=currentUser)),
-    'liked' : Wishlist.objects.filter(liked_users = currentUser)
-  }
-  liked = {}
-  return render(request, 'first_app/dashboard.html', context)
-
-def logout(request):
-    request.session.clear()
-    return redirect('/')
-
-def create(request):
-  return render(request, 'first_app/create.html')
-
-def createrender(request):
-  errors = Wishlist.objects.WishManager(request.POST)
-  if (errors):
-    for key, value in errors.items():
-      messages.error(request, value)
-    return redirect('/wish_items/create')
-  else:
-    Wishlist.objects.create(name = request.POST['name'],added_by = User.objects.get(id=request.session['id']))
-    return redirect('/dashboard')
-
-def destroy(request, number):
-  Wishlist.objects.get(id=number).delete()
-  return redirect('/dashboard')
-
-def wishItem(request, number):
-  user = User.objects.get(id=number)
-  item = Wishlist.objects.get(id=number)
-  context = {
-    'user' : user,
-    'item' : item
-  }
-  return render(request, 'first_app/info.html', context)
-
-# def add(request, number):
-#    User.objects.get(id=request.session['id']).liked_items.add(Wishlist.objects.get(id=number))
-#    return redirect('/dashboard')
-
-# def show(request, number):
-#   context = {
-#     'item' : Wishlist.objects.get(id=itemid),
-#     'likedusers' : Wishlist.objects.get(id=number).liked_users.all().values('first_name')
-#   }
-#   return render(request, 'first_app/info.html', context)
+def success(request):
+  return render(request, 'first_app/success.html')
